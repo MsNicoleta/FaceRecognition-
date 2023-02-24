f15c8f6519c5356069b1894e997df400ced96baf
MsNicoleta <106880559+MsNicoleta@users.noreply.github.com>
2023-02-24 13:37:00 +0000

    file error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Navigation/Logo/logo';
+import Logo from './components/Navigation/Logo/Logo';
 import './App.css';
 
 function App() {
