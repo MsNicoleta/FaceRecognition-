6f2ce07011071a36024bc2d3a7dd34edfc351710
MsNicoleta <106880559+MsNicoleta@users.noreply.github.com>
2023-02-27 11:38:31 +0000

    working on background particles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo/>
-        <ImageLinkForm/>
-     {/* <FaceRecognition/> */}
-    </div>
-  );
+
+
+class App extends Component {
+    render(){
+    return (
+      <div className="App">
+        <ParticlesBg type="circle" bg={true}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './ImageLinkForm.css';
 
 
 const ImageLinkForm = () => {
@@ -8,9 +8,11 @@ const ImageLinkForm = () => {
             <p className='f3'>
                   {'This magic Brain will detect faces in your pictures. Give it a try!'}
             </p>
-            <div className="">
-                <input className="f4 pa2 w-60 center" type="text" />
-                <button className="w-10 grow f4 link ph3 pv2 dib white bg-light-purple"> Detect</button>
+            <div className="center">
+                <div className="form center pa4 br3 shadow-5">
+                    <input className=" f4 pa2 w-70 center" type="text" />
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple br3"> Detect</button>
+                </div>
             </div>
         </div>
     );
