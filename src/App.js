730b201f95aa309f827d21a4233d3ee54d786798
MsNicoleta <106880559+MsNicoleta@users.noreply.github.com>
2023-02-27 21:53:41 +0000

    add the particlesOption

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-
+import ParticlesOptions from './ParticlesOptions';
 
 
 class App extends Component {
     render(){
     return (
       <div className="App">
-        <ParticlesBg type="circle" bg={true}
-        />
+        <ParticlesOptions/>
         <Navigation />
         <Logo />
         <Rank />
