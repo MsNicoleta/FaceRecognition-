529040e877d6becc7614a2b7a8cc30e274a43647
MsNicoleta <106880559+MsNicoleta@users.noreply.github.com>
2023-02-27 23:36:12 +0000

    up to date

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+// import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-import ParticlesOptions from './ParticlesOptions';
+// import ParticlesOptions from './ParticlesOptions';
 
 
 class App extends Component {
     render(){
     return (
       <div className="App">
-        <ParticlesOptions/>
+        {/* <ParticlesOptions/> */}
+        {/* <ParticlesBg type="circle" bg={true} /> */}
         <Navigation />
         <Logo />
         <Rank />
