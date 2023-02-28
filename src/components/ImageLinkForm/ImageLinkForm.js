23e72eb3e23112bfeb6b83de06d3bae705d0a0a0
MsNicoleta <106880559+MsNicoleta@users.noreply.github.com>
2023-02-28 14:26:23 +0000

    add onInputChange

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,7 +10,7 @@ const ImageLinkForm = ({onInputChange}) => {
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className=' f4 pa2 w-70 center' type='tex' onChange={onInp} />
+                    <input className=' f4 pa2 w-70 center' type='tex' onChange={onInputChange} />
                     <button className='w-30 grow f4 link ph3 pv2 dib white bg-dark-blue br3 ba b--white-025'> Detect</button>
                 </div>
             </div>
